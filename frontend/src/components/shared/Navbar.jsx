546bece3f8783f8602b82702a32b91e72f22c100
Rahul Karak <Rahulkarak1354@gmail.com>
2025-01-06 22:40:46 +0530

    Navbar completed

--- frontend/src/components/shared/Navbar.jsx
+++ frontend/src/components/shared/Navbar.jsx
@@ -1,9 +1,11 @@
 import { Popover, PopoverContent, PopoverTrigger, } from "@/components/ui/popover"
 import { Avatar, AvatarImage } from '../ui/avatar'
+import { LogOut, User2 } from 'lucide-react'
 import React from 'react'
 import { Button } from "../ui/button"
 
 const Navbar = () => {
+    const user = false;
     return (
         <div className='bg-white'>
             <div className='flex items-center justify-between mx-auto max-w-7xl h-16'>
@@ -16,24 +18,51 @@ const Navbar = () => {
                         <li>Jobs</li>
                         <li>Browse</li>
                     </ul>
-                    <Popover >
-                        <PopoverTrigger asChild>
-                            <Avatar className="cursor-pointer">
-                                <AvatarImage src="https://github.com/shadcn.png" />
-
-                            </Avatar>
-
-                        </PopoverTrigger>
-                        <PopoverContent className="w-80">
-                            <div>
-                                <Avatar className="cursor-pointer">
-                                    <AvatarImage src="https://github.com/shadcn.png" />
-                                </Avatar>
-                                <h4 className="font-medium">Rahul mern stack</h4>
+                    {
+                        !user ? (
+                            <div className="flex items-center gap-2">
+                                <Button variant="outline">Login</Button>
+                                <Button className="bg-[#6A38C2] hover:bg-[#5426a2]">Signup</Button>
                             </div>
+                        ) : (
+                            <Popover >
+                                <PopoverTrigger asChild>
+                                    <Avatar className="cursor-pointer">
+                                        <AvatarImage src="https://github.com/shadcn.png" />
 
-                        </PopoverContent>
-                    </Popover>
+                                    </Avatar>
+
+                                </PopoverTrigger>
+                                <PopoverContent className="w-80">
+                                    <div>
+                                        <div className="flex gap-2 space-y-2">
+                                            <Avatar className="cursor-pointer">
+                                                <AvatarImage src="https://github.com/shadcn.png" />
+                                            </Avatar>
+                                            <div>
+                                                <h4 className="font-medium">Rahul mern stack</h4>
+                                                <p className="text-small text-muted-foreground">Lorem ipsum dolor sit amet.</p>
+                                            </div>
+                                        </div>
+                                        <div className="flex flex-col my-2 text-gray-600">
+
+                                            <div className="flex w-fit items-center gap-2 cursor-pointer">
+                                                <User2 />
+                                                <Button variant="link">View Profile</Button>
+                                            </div>
+                                            <div className="flex w-fit items-center gap-2 cursor-pointer">
+                                                <LogOut />
+                                                <Button variant="link">Logout</Button>
+                                            </div>
+                                        </div>
+                                    </div>
+
+
+                                </PopoverContent>
+                            </Popover>
+                        )
+                    }
+
                 </div>
             </div>
 
