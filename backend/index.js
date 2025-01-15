6b064cff5da97373e8d871137090acc5d0105436
Rahul Karak <Rahulkarak1354@gmail.com>
2025-01-15 17:13:53 +0530

    fixed typo

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ const PORT = process.env.PORT || 3000;
 
 const app = express();
 
-const _dirname = path.resolve();
+const __dirname = path.resolve();
 
 //middleware
 app.use(express.json());
@@ -40,9 +40,9 @@ app.use("/api/v1/company", companyRoute);
 app.use("/api/v1/job", jobRoute);
 app.use("/api/v1/application", applicationRoute);
 
-app.use(express.static(path.join(_dirname, "/frontend/dist")));
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
 app.get('*', (_, res) => {
-    res.sendFile(path.resolve(_dirname, "frontend", "dist", "index.html"));
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
 });
 
 
