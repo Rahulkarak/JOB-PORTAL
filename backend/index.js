b9b614e6177b2e60eb55b4783bc37c3287736acf
Rahul Karak <Rahulkarak1354@gmail.com>
2025-01-15 16:04:38 +0530

    health added

--- backend/index.js
+++ backend/index.js
@@ -12,41 +12,43 @@ import path from "path"
 dotenv.config({});
 
 connectDB();
-const PORT=process.env.PORT||3000;
+const PORT = process.env.PORT || 3000;
 
-const app=express();
+const app = express();
 
-const _dirname=path.resolve();
+const _dirname = path.resolve();
 
 //middleware
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser())
 
-const corsOptions={
+const corsOptions = {
     // origin:'http://localhost:5173',
     origin: ['http://localhost:5173', 'https://job-portal-69ga.onrender.com'],
-    credentials:true
+    credentials: true
 }
 app.use(cors(corsOptions));
 
 
-
+app.use("/health", (req,res) => {
+    res.status(200).json({ message: "health is good " })
+})
 //api
-app.use("/api/v1/user",userRoute);
-app.use("/api/v1/company",companyRoute);
-app.use("/api/v1/job",jobRoute);
-app.use("/api/v1/application",applicationRoute);
+app.use("/api/v1/user", userRoute);
+app.use("/api/v1/company", companyRoute);
+app.use("/api/v1/job", jobRoute);
+app.use("/api/v1/application", applicationRoute);
 
-app.use(express.static(path.join(_dirname,"/frontend/dist")));
-app.get('*',(_,res)=>{
-    res.sendFile(path.resolve(_dirname,"frontend","dist","index.html"));
+app.use(express.static(path.join(_dirname, "/frontend/dist")));
+app.get('*', (_, res) => {
+    res.sendFile(path.resolve(_dirname, "frontend", "dist", "index.html"));
 });
 
 
 
-app.listen(PORT,()=>{
-    
+app.listen(PORT, () => {
+
     console.log(`Server running at port ${PORT}`);
 })
 
