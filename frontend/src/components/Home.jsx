9bee4eb5fc62a8ac7257410a7cfb30778d7ca017
Rahul Karak <Rahulkarak1354@gmail.com>
2025-01-15 13:21:50 +0530

    fixed some import issue

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -4,7 +4,7 @@ import HeroSection from './HeroSection'
 import CategoryCarousel from './CategoryCarausel'
 import LatestJobs from './LatestJobs'
 import Footer from './shared/Footer'
-import useGetAllJobs from '../hooks/useGetallJobs'
+import useGetAllJobs from '../hooks/useGetAllJobs'
 import { useSelector } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
 
